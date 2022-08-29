9e49f2c6c08eee33a92e0041686e42151dd0c7db
hemantboora <72610847+hemantboora@users.noreply.github.com>
2022-08-29 11:45:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-let bill=document.getElementById("bill").value;
-let tip=document.getElementById("tip").value;
-let person=document.getElementById("person").value;
-
-
 const btn=document.getElementById("btn");
 btn.addEventListener("click",myfun);
 
 function myfun(){
-    alert("hello");
-    alert(bill);
-    alert(tip);alert(person)
-    console.log(bill);
-    console.log(tip);
-    console.log(person);
+    let bill=document.getElementById("bill").value;
+    let tip=document.getElementById("tip").value;
+    let person=document.getElementById("person").value;
+    bill=Number(bill);
+    tip=Number(tip);
+    person=Number(person);
+    let totalBill=((bill*tip)/100)+bill;
+    
+    let perPerson=(bill*(tip/100))/person;
+    document.getElementById("tipPerPerson").value="Tip "+perPerson+" Rs";
+    
+    let billPerPerson=totalBill/person;
+    document.getElementById("totalBillPerPerson").value="Bill Per Head "+billPerPerson+" Rs";
+    
+    
+    document.getElementById("TotalOutput").value="TotalBill "+totalBill+" Rs";
 }
 
